aa493363b02b726ed18d1ca3dd4c3d93cf9ced04
AngieH <62782529+htbngoc@users.noreply.github.com>
2022-07-01 12:42:04 -0700

    change function addproduct

--- action.js
+++ action.js
@@ -19,7 +19,11 @@ function makeOrder() {
 function addProduct(event) {
   event.preventDefault;
   let quantity = prompt("How many boxes do you want to buy?");
-  alert(quantity + " box of 140gr x 24 has added to your basket!");
+  if (quantity > 0) {
+    alert(quantity + " box of 140gr x 24 has added to your basket!");
+  } else {
+    alert("Let's order something!");
+  }
 }
 
 let shoppingCart = document.querySelector("#shopping-button");
@@ -30,3 +34,18 @@ orderEvent.addEventListener("click", makeOrder);
 
 let addProductEvent = document.querySelector("#add-product");
 addProductEvent.addEventListener("click", addProduct);
+
+let addProductEvent1 = document.querySelector("#add-product-1");
+addProductEvent1.addEventListener("click", addProduct);
+
+let addProductEvent2 = document.querySelector("#add-product-2");
+addProductEvent2.addEventListener("click", addProduct);
+
+let addProductEvent3 = document.querySelector("#add-product-3");
+addProductEvent3.addEventListener("click", addProduct);
+
+let addProductEvent4 = document.querySelector("#add-product-4");
+addProductEvent4.addEventListener("click", addProduct);
+
+let addProductEvent5 = document.querySelector("#add-product-5");
+addProductEvent5.addEventListener("click", addProduct);
